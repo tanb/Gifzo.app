38a4cf05e4304ce59cacb8d6b409c36be649d912
Kazato Sugimoto <uiureo@gmail.com>
2013-06-21 14:03:56 +0900

    set max frame rate to 30

--- Gifzo/Recorder.m
+++ Gifzo/Recorder.m
@@ -29,7 +29,7 @@
     AVCaptureScreenInput *input = [[AVCaptureScreenInput alloc] initWithDisplayID:displayID];
     [input setCropRect:NSRectToCGRect(rect)];
     
-    int32_t MAX_FRAME_RATE = 50;
+    int32_t MAX_FRAME_RATE = 30;
     input.minFrameDuration = CMTimeMake(1, MAX_FRAME_RATE);
 
     if ([_captureSession canAddInput:input]) {
